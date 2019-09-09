c9d4b35cf89b4b5bedf58dfc4310c547a97f4fcc
vite <magisterium.vs@list.ru>
2019-09-09 20:30:28 +0300

    Refactoring the CustomTextBoxEditor

--- src/TFlex.PackageManager.UI/Editors/CustomTextBoxEditor.xaml.cs
+++ src/TFlex.PackageManager.UI/Editors/CustomTextBoxEditor.xaml.cs
@@ -1,24 +1,24 @@
 ﻿using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Media;
 using Xceed.Wpf.Toolkit.PropertyGrid;
 using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
 using UndoRedoFramework;
 
 namespace TFlex.PackageManager.Editors
 {
-    public partial class CustomTextBoxEditor : TextBox, ITypeEditor
+    /// <summary>
+    /// Interaction logic for CustomTextBoxEditor.xaml
+    /// </summary>
+    public partial class CustomTextBoxEditor : UserControl, ITypeEditor
     {
         UndoRedo<string> value;
 
         public CustomTextBoxEditor()
         {
-            Padding = new Thickness(3);
-            BorderBrush = Brushes.White;
+            InitializeComponent();
             UndoRedoManager.CommandDone += UndoRedoManager_CommandDone;
         }
 
@@ -30,23 +30,24 @@ namespace TFlex.PackageManager.Editors
             switch (e.CommandDoneType)
             {
                 case CommandDoneType.Undo:
-                    if (UndoRedoManager.RedoCommands.Count() > 0 &&
-                        UndoRedoManager.RedoCommands.Last() == p.PropertyName && Text != value.Value)
+                    if (e.Caption == p.PropertyName)
                     {
-                        Text = value.Value;
+                        Value = value.Value;
+
+                        //Debug.WriteLine(string.Format("Undo: [name: {0}, value: {1}]", 
+                        //    p.PropertyName, p.Value));
                     }
                     break;
                 case CommandDoneType.Redo:
-                    if (UndoRedoManager.UndoCommands.Count() > 0 &&
-                        UndoRedoManager.UndoCommands.Last() == p.PropertyName && Text != value.Value)
+                    if (e.Caption == p.PropertyName)
                     {
-                        Text = value.Value;
+                        Value = value.Value;
+
+                        //Debug.WriteLine(string.Format("Redo: [name: {0}, value: {1}]",
+                        //    p.PropertyName, p.Value));
                     }
                     break;
             }
-
-            //Debug.WriteLine(string.Format("Action: [name: {0}, value: {1}, type: {2}]",
-            //    p.PropertyName, p.Value, e.CommandDoneType));
         }
 
         private static readonly DependencyProperty ValueProperty =
@@ -71,29 +72,28 @@ namespace TFlex.PackageManager.Editors
             };
             BindingOperations.SetBinding(this, ValueProperty, binding);
 
-            Text = Value;
-            LostKeyboardFocus += TextBox_LostKeyboardFocus;
             value = new UndoRedo<string>(Value);
-
+            LostKeyboardFocus += TextBox_LostKeyboardFocus;
+            
             return this;
         }
 
         private void TextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             var p = DataContext as PropertyItem;
-            Value = Text;
+            Value = textBox.Text;
 
-            if (value.Value != Text)
+            if (value.Value != textBox.Text)
             {
                 using (UndoRedoManager.Start(p.PropertyName))
                 {
-                    value.Value = Text;
+                    value.Value = textBox.Text;
                     UndoRedoManager.Commit();
                 }
-            }
 
-            //Debug.WriteLine(string.Format("PropertyItem: [name: {0}, value: {1}, can undo: {2}, can redo: {3}]",
-            //    p.PropertyName, p.Value, UndoRedoManager.CanUndo, UndoRedoManager.CanRedo));
+                //Debug.WriteLine(string.Format("Commit: [name: {0}, value: {1}]", 
+                //    p.PropertyName, p.Value));
+            }
         }
     }
 }
