5bdc1979f66c11bb4ad67b4bdd18bfe31cfd1a3e
vite <magisterium.vs@list.ru>
2020-02-17 12:59:00 +0300

    Updated T-FLEX CAD API to 16.0.61.0 version

--- src/TFPackageManager/ApiLoader.cs
+++ src/TFPackageManager/ApiLoader.cs
@@ -13,7 +13,7 @@ namespace TFlex
     public static class ApiLoader
     {
         #region private fields
-        private const string API_VERSION = "16.0.60.0";
+        private const string API_VERSION = "16.0.61.0";
         private static List<string> folders;
         private static bool isLoaded;
         #endregion
