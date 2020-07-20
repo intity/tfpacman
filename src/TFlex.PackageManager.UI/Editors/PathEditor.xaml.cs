d3344ed50aa41d4d3d94b64889da14d7e6e45406
vite <magisterium.vs@list.ru>
2020-07-20 03:56:28 +0300

    refactoring the resources to PathEditor

--- src/TFlex.PackageManager.UI/Editors/PathEditor.xaml.cs
+++ src/TFlex.PackageManager.UI/Editors/PathEditor.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using TFlex.PackageManager.Common;
 using Xceed.Wpf.Toolkit.PropertyGrid;
 using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
@@ -22,25 +19,16 @@ namespace TFlex.PackageManager.Editors
     public partial class PathEditor : UserControl, ITypeEditor
     {
         #region private fields
-        readonly List<ImageSource> imageSources;
         UndoRedo<string> value;
         #endregion
 
         public PathEditor()
         {
             InitializeComponent();
-
-            imageSources = new List<ImageSource>
-            {
-                new BitmapImage(new Uri(Resource.BASE_URI + "open_configurations.ico")),
-                new BitmapImage(new Uri(Resource.BASE_URI + "open_initial_catalog.ico")),
-                new BitmapImage(new Uri(Resource.BASE_URI + "open_target_directory.ico"))
-            };
-
-            UndoRedoManager.CommandDone += UndoRedoManager_CommandDone;
+            UndoRedoManager.CommandDone += UndoManager_CommandDone;
         }
 
-        private void UndoRedoManager_CommandDone(object sender, CommandDoneEventArgs e)
+        private void UndoManager_CommandDone(object sender, CommandDoneEventArgs e)
         {
             if (!(DataContext is PropertyItem p))
                 return;
@@ -48,15 +36,8 @@ namespace TFlex.PackageManager.Editors
             switch (e.CommandDoneType)
             {
                 case CommandDoneType.Undo:
-                    if (UndoRedoManager.RedoCommands.Count() > 0 &&
-                        UndoRedoManager.RedoCommands.Last() == p.PropertyName && Value != value.Value)
-                    {
-                        Value = value.Value;
-                    }
-                    break;
                 case CommandDoneType.Redo:
-                    if (UndoRedoManager.UndoCommands.Count() > 0 &&
-                        UndoRedoManager.UndoCommands.Last() == p.PropertyName && Value != value.Value)
+                    if (e.Caption == p.PropertyName)
                     {
                         Value = value.Value;
                     }
@@ -88,22 +69,20 @@ namespace TFlex.PackageManager.Editors
                 Mode = propertyItem.IsReadOnly ? BindingMode.OneWay : BindingMode.TwoWay
             };
             BindingOperations.SetBinding(this, ValueProperty, binding);
-
             value = new UndoRedo<string>(Value);
 
             switch (propertyItem.PropertyName)
             {
                 case "UserDirectory":
-                    (buton1.Content as Image).Source = imageSources[0];
+                    (buton1.Content as Image).Source = Resources["icon_1"] as ImageSource;
                     break;
                 case "InitialCatalog":
-                    (buton1.Content as Image).Source = imageSources[1];
+                    (buton1.Content as Image).Source = Resources["icon_2"] as ImageSource;
                     break;
                 case "TargetDirectory":
-                    (buton1.Content as Image).Source = imageSources[2];
+                    (buton1.Content as Image).Source = Resources["icon_3"] as ImageSource;
                     break;
             }
-
             return this;
         }
 
@@ -121,7 +100,7 @@ namespace TFlex.PackageManager.Editors
                 StartupLocation = WindowStartupLocation.CenterOwner
             };
 
-            if (fbd.ShowDialog())
+            if (fbd.ShowDialog() && Value != fbd.SelectedPath)
             {
                 Value = fbd.SelectedPath;
                 using (UndoRedoManager.Start(p.PropertyName))
