df40f5b4798395bb26727158c785d612c4d9c84b
vite <magisterium.vs@list.ru>
2020-11-12 09:50:28 +0300

    moved CustomTreeView and CustomTreeViewItem classes to external files

---
+++ src/TFlex.PackageManager.UI/Controls/CustomTreeView.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TFlex.PackageManager.UI.Controls
+{
+    /// <summary>
+    /// The Custom TreeView class.
+    /// </summary>
+    public class CustomTreeView : TreeView
+    {
+        #region properties
+        /// <summary>
+        /// Enables check boxes for items in a tree-view control.
+        /// </summary>
+        public bool CheckboxesVisible { get; set; }
+        #endregion
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new CustomTreeView();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is CustomTreeViewItem;
+        }
+    }
+}
---
+++ src/TFlex.PackageManager.UI/Controls/CustomTreeViewItem.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TFlex.PackageManager.UI.Controls
+{
+    /// <summary>
+    /// The TreeViewItem class override.
+    /// </summary>
+    public class CustomTreeViewItem : TreeViewItem
+    {
+        #region private fields
+        int level = -1;
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// The IsChecked property to checkbox.
+        /// </summary>
+        public bool? IsChecked
+        {
+            get => GetValue(IsCheckedProperty) as bool?;
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        /// <summary>
+        /// Is the item a node.
+        /// </summary>
+        public bool IsNode
+        {
+            get
+            {
+                return File.GetAttributes(Tag.ToString()) == FileAttributes.Directory;
+            }
+        }
+
+        /// <summary>
+        /// The tree node parent object.
+        /// </summary>
+        public CustomTreeViewItem NodeParent { get; set; }
+
+        /// <summary>
+        /// Item's hierarchy in the tree
+        /// </summary>
+        public int Level
+        {
+            get
+            {
+                if (level == -1)
+                {
+                    level = (ItemsControlFromItemContainer(this) is CustomTreeViewItem parent)
+                        ? parent.Level + 1 : 0;
+                }
+                return level;
+            }
+        }
+
+        /// <summary>
+        /// File extension.
+        /// </summary>
+        public string Extension
+        {
+            get => GetValue(ExtensionProperty) as string;
+            set => SetValue(ExtensionProperty, value);
+        }
+
+        /// <summary>
+        /// The ImageSource property.
+        /// </summary>
+        public ImageSource ImageSource
+        {
+            get => GetValue(ImageSourceProperty) as ImageSource;
+            set => SetValue(ImageSourceProperty, value);
+        }
+        #endregion
+
+        #region public fields
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.RegisterAttached("IsChecked",
+                typeof(bool?),
+                typeof(CustomTreeViewItem),
+                new FrameworkPropertyMetadata((bool?)false));
+
+        public static readonly DependencyProperty ExtensionProperty =
+            DependencyProperty.Register("Extension",
+                typeof(string),
+                typeof(CustomTreeViewItem),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource",
+                typeof(ImageSource),
+                typeof(CustomTreeViewItem),
+                new PropertyMetadata(null));
+        #endregion
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new CustomTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is CustomTreeViewItem;
+        }
+    }
+}
