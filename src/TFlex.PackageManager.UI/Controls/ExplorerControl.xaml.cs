031b88a0d91bdeafc3b4c7400299bbba606bc113
vite <magisterium.vs@list.ru>
2021-07-19 13:42:43 +0300

    refactoring the ExplorerControl

--- src/TFlex.PackageManager.UI/Controls/ExplorerControl.xaml.cs
+++ src/TFlex.PackageManager.UI/Controls/ExplorerControl.xaml.cs
@@ -208,7 +208,7 @@ namespace TFlex.PackageManager.UI.Controls
                 else
                     ctv1.Items.Add(subItem);
 
-                InitTreeItems_1(subItem);
+                InitTreeItems_1(subItem); // recursive call
             }
 
             GetFiles(item);
@@ -218,7 +218,7 @@ namespace TFlex.PackageManager.UI.Controls
         {
             var dir   = item != null ? item.Tag.ToString() : rootDirectory;
             var opt   = SearchOption.TopDirectoryOnly;
-            var files = Directory.GetFiles(dir, SearchPattern, opt);
+            var files = Directory.GetFiles(dir, searchPattern, opt);
 
             foreach (var i in files)
             {
@@ -234,11 +234,15 @@ namespace TFlex.PackageManager.UI.Controls
 
         private void InitTreeItems_2()
         {
+            //
+            // Initializing a tree structure of assembly units and related 
+            // documents.
+            //
             if (searchPattern != "*.grb")
                 return;
 
             var opt   = SearchOption.TopDirectoryOnly;
-            var files = Directory.GetFiles(rootDirectory, SearchPattern, opt);
+            var files = Directory.GetFiles(rootDirectory, searchPattern, opt);
             foreach (var i in files)
             {
                 var item = CreateItem(i);
@@ -249,19 +253,20 @@ namespace TFlex.PackageManager.UI.Controls
 
         private void GetLinks(CustomTreeViewItem item)
         {
-            var obj   = item.Tag as ProcItem;
-            var path  = Flags > 0 ? obj.OPath : obj.IPath;
+            var data  = item.Tag as ProcItem;
+            var path  = Flags > 0 ? data.OPath : data.IPath;
             var links = Application
-                .GetDocumentExternalFileLinks(path, true, false, false);
+                .GetDocumentExternalFileLinks(path, true, false, false)
+                .OrderBy(i => i);
 
-            foreach (var link in links.OrderBy(i => i))
+            foreach (var i in links)
             {
-                var subItem = CreateItem(link);
-                var objLink = subItem.Tag as ProcItem;
-                objLink.Parent = obj;
-                obj.Items.Add(objLink);
+                var subItem = CreateItem(i);
+                var subData = subItem.Tag as ProcItem;
+                subData.Parent = data;
+                data.Items.Add(subData);
                 item.Items.Add(subItem);
-                GetLinks(subItem);
+                GetLinks(subItem); // recursive call
             }
 
             Application.IdleSession();
@@ -286,78 +291,144 @@ namespace TFlex.PackageManager.UI.Controls
             }
         }
 
-        private void SelectedItemTask(ProcItem item, string path, ref bool res)
+        private void SelectedItemTask(ProcItem item)
         {
-            if (item.IPath == path)
-            {
-                item.Flags |= 0x1;
-                res = true;
-                return;
-            }
-
             foreach (var i in item.Items)
             {
-                SelectedItemTask(i, path, ref res);
+                if (item.Flags == 0)
+                {
+                    int result = 0;
+                    var parent = item.Parent;
+                    while (parent != null)
+                    {
+                        result = parent.Flags;
+                        parent = parent.Parent;
+                    }
+                    i.Flags = result;
+                }
+                SelectedItemTask(i); // recursive call
+            }
+        }
+
+        private void SelectedItemTask(ProcItem src, ProcItem dst, ref int res)
+        {
+            if (dst.IPath == src.IPath && dst.Level != src.Level)
+            {
+                if (res > 0)
+                {
+                    if (dst.Parent == null || dst.Parent.Flags > 0)
+                    {
+                        dst.Flags = res;
+                        res = 0; // inverted result (1 -> 0)
+                    }
+                }
+                else
+                {
+                    dst.Flags = res;
+                    res |= 0x1; // (0 -> 1)
+                }
+            }
+
+            foreach (var i in dst.Items)
+            {
+                SelectedItemTask(src, i, ref res); // recursive call
+            }
+        }
+
+        private void SelectedItemTask(ProcItem data, int value)
+        {
+            int result = value;
+            data.Flags = value;
+            //
+            // pre-processing the flags
+            //
+            foreach (var item in SelectedItems.Values)
+            {
+                SelectedItemTask(data, item, ref result);
+                if (result != value)
+                {
+                    //
+                    // selection sequence: asm -> part
+                    //
+                    data.Flags = result;
+                    result = value;
+                }
+
+                SelectedItemTask(item, data, ref result);
+                if (result != value)
+                {
+                    //
+                    // selection sequence: part -> asm
+                    //
+                    item.Flags = result;
+                    result = value;
+                }
             }
         }
 
         private void SelectedItemTask(CustomTreeViewItem item, int flag)
         {
-            if (!(item.Tag is ProcItem obj1))
+            if (!(item.Tag is ProcItem data))
                 return;
 
+            ProcItem d;
             if (flag > 0)
             {
-                if (searchPattern == "*.grb")
+                int value = 0x1;
+                if (searchPattern == "*.grb" && (d = GetData(item)) != null)
                 {
-                    var obj2 = GetItem(item);
-                    if (obj2 != null)
-                        obj1 = obj2;
-
-                    foreach (var i in SelectedItems)
-                    {
-                        bool result = false;
-
-                        // selection sequence: asm -> part
-                        SelectedItemTask(i.Value, obj1.IPath, ref result);
-
-                        if (result)
-                            break;
-
-                        // selection sequence: part -> asm
-                        SelectedItemTask(obj1, i.Value.IPath, ref result);
-                    }
+                    data = d;
+                    SelectedItemTask(data, value);
                 }
+                else
+                    data.Flags = value;
 
-                obj1.Flags |= 0x1;
-                SelectedItems.Add(obj1.IPath, obj1);
+                SelectedItems.Add(data.IPath, data);
             }
             else
-                SelectedItems.Remove(obj1.IPath);
+            {
+                SelectedItems.Remove(data.IPath);
+
+                if (searchPattern == "*.grb" && (d = GetData(item)) != null)
+                {
+                    data = d;
+                    SelectedItemTask(data, 0);
+                }
+            }
         }
 
-        private ProcItem GetItem(CustomTreeViewItem item)
+        private ProcItem GetData(CustomTreeViewItem item)
         {
+            //
+            // Getting data by a selected Item from the assembly tree.
+            //
             foreach (CustomTreeViewItem i in ctv2.Items)
             {
                 var value = Compare(item, i);
                 if (value != null)
                     return value;
             }
+
             return null;
         }
 
         private ProcItem Compare(CustomTreeViewItem src, CustomTreeViewItem dst)
         {
-            if ((src.Tag as ProcItem).IPath == (dst.Tag as ProcItem).IPath)
-                return dst.Tag as ProcItem;
+            var data1 = src.Tag as ProcItem;
+            var data2 = dst.Tag as ProcItem;
+
+            if (data1.IPath == data2.IPath && data1.Level == data2.Level)
+            {
+                return data2;
+            }
 
             foreach (CustomTreeViewItem i in dst.Items)
             {
-                var value = Compare(src, i);
+                var value = Compare(src, i); // recursive call
                 if (value != null)
                     return value;
             }
+
             return null;
         }
         #endregion
